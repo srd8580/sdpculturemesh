4cf29c28fb9a5e55de31eb9d12d2067be1c3c9da
Steve Danner <steve.danner@softwaredesignpartners.com>
2018-10-13 16:53:44 -0500

    checkin

---
+++ data/populations/Import-Populations.ps1
@@ -0,0 +1,36 @@
+﻿$sourceDir = "."
+$sourceFiles = Get-ChildItem $sourceDir "*.csv"
+$sqlTemplate = "INSERT INTO AllPopulations (MetroArea,Origin,EthnicPopulation,MetroPopulation) VALUES ('{{metroArea}}', '{{origin}}',{{ethnicPopulation}},{{metroPopulation}});"
+$server = "(local)"
+$outputFile = ".\insertPopulations.sql"
+
+if (Test-Path $outputFile) {
+    Remove-Item $outputFile
+}
+
+$originIdx = 1
+$ethnicPopulationIdx = 9
+$metroPopulationIdx = 12
+
+$sourceFiles | ForEach-Object {
+    $lineCounter = 0
+
+    Get-Content $_ | ForEach-Object {
+        if ($lineCounter -gt 0) {
+            $metro = $_.Substring(0, $_.Trim('"').IndexOf('"') + 2).Trim('"')
+            $content = $_.Substring($metro.Length + 2).Split(',')
+
+            $sql = $sqlTemplate.Replace("{{metroArea}}", $metro.Replace("'", "''"))
+            $sql = $sql.Replace("{{origin}}", $content[$originIdx].Trim('"').Replace("'", "''"))
+            $sql = $sql.Replace("{{ethnicPopulation}}", $content[$ethnicPopulationIdx].Trim('"').Replace("'", "''"))
+            $sql = $sql.Replace("{{metroPopulation}}", $content[$metroPopulationIdx].Trim('"').Replace("'", "''"))
+
+            Add-Content $outputFile $sql
+        }
+
+        $lineCounter++
+    }
+
+}
+
+
