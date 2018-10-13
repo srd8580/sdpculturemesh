8b2bdd6ef021c7135f539d9c114dfd546773e21c
Steve Danner <steve.danner@softwaredesignpartners.com>
2018-10-13 17:14:01 -0500

    checkin

--- scripts/list-resources.js
+++ scripts/list-resources.js
@@ -1,12 +1,11 @@
 app.controller('listResourcesCtrl', function($scope, $http, $routeParams, $location, $window, $timeout) {
-    const googleApiKey = "AIzaSyD4p84joe5raTzMsYWPxBVz6snHBkVQ70I";
-
+    const listUrl = "https://api.mlab.com/api/1/databases/globalhack7/collections/Enclaves?apiKey=QUkNcQwk7nIkMruEKg_2kBb6eQa2WR8J";
 
     $scope.isBusy = false;
     $scope.city = $routeParams["city"];
     $scope.state = $routeParams["state"];
     $scope.ethnicity = $routeParams["ethnicity"];
-
+    $scope.resources = [];
 
     if (!$scope.city || !$scope.state || !$scope.ethnicity) {
         $location.path("/");
@@ -14,15 +13,31 @@ app.controller('listResourcesCtrl', function($scope, $http, $routeParams, $locat
     }
     
     $scope.init = function () {
+        var queryParameters = '&q={ "$and": [ {"ethnicity": "' + $scope.ethnicity + '"}, {"location.city": "' + $scope.city + '"}, { "location.state": "' + $scope.state + '" }] }';
 
         $scope.isBusy = true;
 
-        $timeout(function () {
-            $scope.isBusy = false;
-        }, 1000);
+        $http({
+            method: 'GET',
+            url: listUrl + queryParameters
+        })
+            .then(function success (response) {
+                if (response.data && response.data.length)
+                    $scope.resources = response.data[0].resources;
+                else
+                    $scope.resources = [];
+            }, function error (err) {
+                $window.alert("Error loading resources!");
+            })
+            .then(function () {
+                $scope.isBusy = false;
+            });
     };
 
-
+    $scope.onRowClick = function (resource) {
+        var url = "https://www.google.com/search?q=" + escape(resource.name) + " " + escape(resource.address || "") + " " + escape(resource.zip || "");
+        $window.open(url, "_blank");
+    };
 
     $scope.init();
 });
